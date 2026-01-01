73c0fddbeaff7d433af512c055b2e24e5f418c8a
Yunseonghwan <sunghwan0208@naver.com>
2026-01-01 18:53:37 +0900

    여행 업데이트 api연동

--- app/(trip)/updateTrip/index.tsx
+++ app/(trip)/updateTrip/index.tsx
@@ -1,13 +1,39 @@
 import TripForm from "@/components/TripForm";
-import { useState } from "react";
+import { useGetTrip, useUpdateTrip } from "@/hooks/useTrip";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 
 const UpdateTripScreen = () => {
+  const router = useRouter();
+  const { tripId } = useLocalSearchParams<{ tripId: string }>();
   const [title, setTitle] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
+  const { data } = useGetTrip(tripId);
+  const { mutateAsync } = useUpdateTrip();
+
+  useEffect(() => {
+    if (data) {
+      setTitle(data.title);
+      setStartDate(new Date(data.startDate));
+      setEndDate(new Date(data.endDate));
+    }
+  }, [data]);
+
   const handleTitleChange = (text: string) => {
     setTitle(text);
+  };
+
+  const handleUpdateTrip = () => {
+    mutateAsync(
+      { tripId, title, startDate, endDate },
+      {
+        onSuccess: () => {
+          router.back();
+        },
+      }
+    );
   };
 
   return (
@@ -18,7 +44,7 @@ const UpdateTripScreen = () => {
       handleTitleChange={handleTitleChange}
       setStartDate={setStartDate}
       setEndDate={setEndDate}
-      onPress={() => null}
+      onPress={handleUpdateTrip}
     />
   );
 };
--- hooks/useTrip.tsx
+++ hooks/useTrip.tsx
@@ -1,10 +1,16 @@
 import { api } from "@/api";
-import { CreateTripRequestType, TripListResponseType } from "@/types/tripTypes";
+import {
+  CreateTripRequestType,
+  TripItemType,
+  TripListResponseType,
+} from "@/types/tripTypes";
 import {
   useInfiniteQuery,
   UseInfiniteQueryResult,
   useMutation,
+  useQuery,
   useQueryClient,
+  UseQueryResult,
 } from "@tanstack/react-query";
 
 export const useCreateTrip = () => {
@@ -38,6 +44,33 @@ export const useGetTrips = (): UseInfiniteQueryResult<{
         return lastPage.meta.currentPage + 1;
       }
       return undefined;
+    },
+  });
+};
+
+export const useGetTrip = (tripId: string): UseQueryResult<TripItemType> => {
+  return useQuery({
+    queryKey: ["trip", tripId],
+    queryFn: async () => {
+      const res = await api.get(`/trips/${tripId}`);
+      return res.data;
+    },
+  });
+};
+
+export const useUpdateTrip = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (body: CreateTripRequestType & { tripId: string }) => {
+      const res = await api.patch(`/trips/${body.tripId}`, {
+        title: body.title,
+        startDate: body.startDate,
+        endDate: body.endDate,
+      });
+      return res.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["trips"] });
     },
   });
 };
